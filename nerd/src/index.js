7061f0221067315bc9ec4607877849c39ea4cefd
Eirik Årdal <eaardal@outlook.com>
2017-09-17 15:06:15 +0200

    Exercise 3 implemented

--- nerd/src/index.js
+++ nerd/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './reduxStore';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(
+  <Provider store={store}>
+      <div>
+        <App />
+      </div>
+  </Provider>,
+  document.getElementById('root')
+);
